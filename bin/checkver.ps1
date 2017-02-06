73d4bacd46526a686059bdb374a810c4d92c2aab
Richard Kuhnt <r15ch13+git@gmail.com>
2017-02-06 22:21:44 +0100

    Fixes manifest test and enable -u in checkver

--- bin/checkver.ps1
+++ bin/checkver.ps1
@@ -1,7 +1,14 @@
-param($app) #optionally pass an app name to just check one app
+param(
+    [String]$app, #optionally pass an app name to just check one app
+    [Switch]$update = $false
+)
 
 $bin = split-path (scoop which scoop)
 $checkver = "$bin\checkver.ps1"
 $dir = "$psscriptroot\.." # checks the parent dir
 
-& $checkver -app $app -dir $dir
+if($update) {
+    & $checkver -app $app -dir $dir -u
+} else {
+    & $checkver -app $app -dir $dir
+}
