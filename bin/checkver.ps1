a47414862a3df0293394c90e87437216b5420920
Richard Kuhnt <r15ch13+git@gmail.com>
2017-02-07 19:35:16 +0100

    Improve checkver

--- bin/checkver.ps1
+++ bin/checkver.ps1
@@ -1,14 +1,4 @@
-param(
-    [String]$app, #optionally pass an app name to just check one app
-    [Switch]$update = $false
-)
-
 $bin = split-path (scoop which scoop)
 $checkver = "$bin\checkver.ps1"
 $dir = "$psscriptroot\.." # checks the parent dir
-
-if($update) {
-    & $checkver -app $app -dir $dir -u
-} else {
-    & $checkver -app $app -dir $dir
-}
+iex -command "$checkver -dir $dir $($args |% { "$_ " })"
