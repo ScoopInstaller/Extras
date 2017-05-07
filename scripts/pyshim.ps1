3d912e1da312ca8f5470876aa70013255754717f
Richard Kuhnt <r15ch13+git@gmail.com>
2017-05-07 19:05:13 +0200

    Remove UTF-8 BOM, replace tabs withs, add newline at EOF, format json

--- scripts/pyshim.ps1
+++ scripts/pyshim.ps1
@@ -1,7 +1,7 @@
 param($dir)
 
 ls "$dir" | ? name -match '^[^_].*?\.py$' | % {
-	$n = $_.name -replace '\.py$', ''
-	$ps = "$dir\$n.ps1"
-	"& python `"`$psscriptroot\$n.py`" @args" > $ps
+    $n = $_.name -replace '\.py$', ''
+    $ps = "$dir\$n.ps1"
+    "& python `"`$psscriptroot\$n.py`" @args" > $ps
 }
