f8f6316e375ad92745f2e73a66bfcad60adc290a
Xianyi Cui <cuixianyi@gmail.com>
2019-11-15 22:09:02 +0800

    remote unneeded file

--- scripts/common/check-permission.ps1
+++
@@ -1,7 +0,0 @@
-$identity = [Security.Principal.WindowsIdentity]::GetCurrent()
-$principal = New-Object Security.Principal.WindowsPrincipal -ArgumentList $identity
-if ($principal.IsInRole( [Security.Principal.WindowsBuiltInRole]::Administrator )){
-  Write-Host 'You have Admin privilege. Continue for installation...'
-} else {
-  throw 'Check privilege fail. Please run installation under admin privilege...'
-}
