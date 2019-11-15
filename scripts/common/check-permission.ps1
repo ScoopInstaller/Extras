2b31acaa1c1c8903f06bb070b114c1d9d738b3ed
Xianyi Cui <cuixianyi@gmail.com>
2019-11-15 21:38:13 +0800

    Update check-permission.ps1

--- scripts/common/check-permission.ps1
+++ scripts/common/check-permission.ps1
@@ -1,6 +1,6 @@
 $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
 $principal = New-Object Security.Principal.WindowsPrincipal -ArgumentList $identity
-if (ncipal.IsInRole( [Security.Principal.WindowsBuiltInRole]::Administrator )){
+if (principal.IsInRole( [Security.Principal.WindowsBuiltInRole]::Administrator )){
   Write-Host 'You have Admin privilege. Continue for installation...'
 } else {
   throw 'Check privilege fail. Please run installation under admin privilege...'
