0b287c611b6fac239298e10adafa0da8d8879745
dkgdsc <1147554946@qq.com>
2021-10-04 15:53:23 +0800

    delel

--- Scoop-Bucket.Tests.ps1
+++
@@ -1,2 +0,0 @@
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
-. "$env:SCOOP_HOME\test\Import-Bucket-Tests.ps1"
--- bin/test.ps1
+++
@@ -1,2 +0,0 @@
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
-Invoke-Pester "$psscriptroot/.."
