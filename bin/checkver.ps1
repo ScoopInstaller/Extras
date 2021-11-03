daafb81d5cf29d4e1518a8b58dfc83e0a8a12b61
Hsiao-nan Cheung <niheaven@gmail.com>
2021-11-03 14:37:41 +0800

    fix(checkver): use `scoop prefix` to correctly find scoop's path (#7223)

--- bin/checkver.ps1
+++ bin/checkver.ps1
@@ -1,4 +1,4 @@
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
+if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop prefix scoop))) }
 $checkver = "$env:SCOOP_HOME/bin/checkver.ps1"
 $dir = "$psscriptroot/../bucket" # checks the parent dir
 Invoke-Expression -command "& '$checkver' -dir '$dir' $($args | ForEach-Object { "$_ " })"
