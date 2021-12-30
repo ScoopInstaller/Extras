91a9eb638b43910e1531730a68affa995fc5b043
Hsiao-nan Cheung <niheaven@gmail.com>
2021-12-31 00:16:18 +0800

    fix(test): Use new manifests test

--- Scoop-Bucket.Tests.ps1
+++
@@ -1,2 +0,0 @@
-if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
-. "$env:SCOOP_HOME\test\Import-Bucket-Tests.ps1"
--- bin/test.ps1
+++ bin/test.ps1
@@ -1,2 +1,4 @@
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
-Invoke-Pester "$psscriptroot/.."
+#Requires -Modules @{ ModuleName = 'Pester'; RequiredVersion = '4.10.1' }
+
+if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
+Invoke-Pester @{ Path = "$env:SCOOP_HOME\test\Import-Bucket-Tests.ps1"; Arguments = "$PSScriptRoot/.." }
