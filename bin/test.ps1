49aa226e80c87f69fdbcdbb70c57e5d5159cbb2f
Rashil Gandhi <46838874+rashil2000@users.noreply.github.com>
2022-03-09 15:22:44 +0530

    Update test.ps1

--- bin/test.ps1
+++ bin/test.ps1
@@ -1,3 +1,5 @@
+#Requires -Modules @{ ModuleName = 'Pester'; MaximumVersion = '4.99' }
+
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
 $result = Invoke-Pester "$psscriptroot/.." -PassThru
 exit $result.FailedCount
