56c8b87add5b9ddff39fe8216ec204106fc82dcc
Rashil Gandhi <46838874+rashil2000@users.noreply.github.com>
2022-03-09 13:27:32 +0530

    Update test.ps1

--- bin/test.ps1
+++ bin/test.ps1
@@ -1,2 +1,3 @@
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
-Invoke-Pester "$psscriptroot/.."
+$result = Invoke-Pester "$psscriptroot/.." -PassThru
+exit $result.FailedCount
