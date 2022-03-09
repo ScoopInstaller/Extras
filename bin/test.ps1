35da51679befbd7e002e7df030f5994a6f120913
Rashil Gandhi <46838874+rashil2000@users.noreply.github.com>
2022-03-09 21:57:47 +0530

    (chore): Migrate CI to GitHub Actions (#8124)
    * Delete appveyor.yml
    
    * Create ci.yml
    
    * Update test.ps1
    
    * Update README.md
    
    * Update test.ps1
    
    * Apply suggestions from code review
    
    Co-authored-by: Hsiao-nan Cheung <niheaven@gmail.com>
    
    Co-authored-by: Hsiao-nan Cheung <niheaven@gmail.com>

--- bin/test.ps1
+++ bin/test.ps1
@@ -1,2 +1,5 @@
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
-Invoke-Pester "$psscriptroot/.."
+#Requires -Modules @{ ModuleName = 'Pester'; MaximumVersion = '4.99' }
+
+if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
+$result = Invoke-Pester "$PSScriptRoot\.." -PassThru
+exit $result.FailedCount
