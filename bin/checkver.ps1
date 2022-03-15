2e3f3a82d7f8aa5e6780853af2ef43507fe92549
Hsiao-nan Cheung <niheaven@gmail.com>
2022-03-15 22:02:20 +0800

    (chore): Change to '$PSScriptRoot'

--- bin/checkver.ps1
+++ bin/checkver.ps1
@@ -1,4 +1,4 @@
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
 $checkver = "$env:SCOOP_HOME/bin/checkver.ps1"
-$dir = "$psscriptroot/../bucket" # checks the parent dir
+$dir = "$PSScriptRoot/../bucket" # checks the parent dir
 Invoke-Expression -command "& '$checkver' -dir '$dir' $($args | ForEach-Object { "$_ " })"
