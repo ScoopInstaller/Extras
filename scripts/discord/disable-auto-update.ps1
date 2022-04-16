f9b655a43a1f5c20f87eae24eea2d3fd09879270
Mathew Block <mathew.block@gmail.com>
2022-04-16 12:50:19 +1000

    Removed unused discord update script

--- scripts/discord/disable-auto-update.ps1
+++
@@ -1,9 +0,0 @@
-$path = "$Env:appdata/discord/settings.json"
-$settings = Get-Content $path | ConvertFrom-Json
-if (Get-Member -inputobject $settings -name 'SKIP_HOST_UPDATE' -Membertype Properties) {
-    $settings.SKIP_HOST_UPDATE=$True
-}
-else {
-    $settings | Add-Member -MemberType NoteProperty -Name 'SKIP_HOST_UPDATE' -Value $True
-}
-$settings | ConvertTo-Json | Set-Content -Path $path
