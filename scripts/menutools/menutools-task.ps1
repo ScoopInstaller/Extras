4e07d021a4b554008d0c63497b71a5ff2c4b6295
Kris Schneider <101912712+Zliced13@users.noreply.github.com>
2022-12-24 21:41:39 -0700

    menutools: Add version 1.0.2

---
+++ scripts/menutools/menutools-task.ps1
@@ -0,0 +1,25 @@
+function is_admin {
+    $admin = [Security.Principal.WindowsBuiltInRole]::Administrator
+    $id = [Security.Principal.WindowsIdentity]::GetCurrent()
+    ([Security.Principal.WindowsPrincipal]($id)).IsInRole($admin)
+}
+
+$subCommand = $args[0]
+
+if ($subCommand -eq 'add') {
+    if (is_admin) {
+        $actions = New-ScheduledTaskAction -Execute "REPLACE_HERE\MenuTools.exe"
+        $triggers = New-ScheduledTaskTrigger -AtStartup -RandomDelay 2
+        $principal = New-ScheduledTaskPrincipal -RunLevel 'Highest' -LogonType 'Interactive' -ProcessTokenSidType 'Default' -UserId $env:USERNAME
+        $settings = New-ScheduledTaskSettingsSet -Compatibility 'Win8' -MultipleInstances 'IgnoreNew'
+        $task = New-ScheduledTask -Description 'Launches MenuTools at startup' -Action $actions -Trigger $triggers -Principal $principal -Settings $settings
+        Register-ScheduledTask -TaskName 'MenuTools' -TaskPath '\' -InputObject $task
+    } elseif (!(is_admin)) {
+        Write-Host "Please Run this Powershell script with Administrator privellages" -ForegroundColor 'Red'
+    }
+} elseif ($subCommand -eq 'remove') {
+    schtasks.exe /delete /tn 'MenuTools' /f
+} elseif ($subCommand -in @('help', $null)) {
+    $message = @("Command usage:`n", "`n`thelp`t`tOpens this help menu.", "`n`tadd`t`tAdds MenuTools as a startup task.", "`n`tremove`t`tRemoves the MenuTools startup task.")
+    Write-Host $message
+}
