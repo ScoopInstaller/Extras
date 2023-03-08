4fbdcd4597508737711e68be490026363bb7d630
Kris Schneider <101912712+Zliced13@users.noreply.github.com>
2023-03-08 13:48:26 -0700

    harmonoid: Fixed install-harmonoid-associations.ps1 so it can correctly install associations (#10677)

--- scripts/harmonoid/install-harmonoid-associations.ps1
+++ scripts/harmonoid/install-harmonoid-associations.ps1
@@ -32,5 +32,6 @@ $formats | ForEach-Object {
     New-Item "HKCU:\SOFTWARE\Classes\Harmonoid.$_\DefaultIcon" -Value "CURRENT_DIRECTORY\harmonoid.exe,0" -Force | Out-Null
     New-Item "HKCU:\SOFTWARE\Classes\Harmonoid.$_\shell\open\command" -Value "CURRENT_DIRECTORY\harmonoid.exe ""%1""" -Force | Out-Null
     New-ItemProperty 'HKCU:\SOFTWARE\Classes\Applications\harmonoid.exe\SupportedTypes' -Name ".$_" -Value '' -Force | Out-Null
+    New-Item "HKCU:\SOFTWARE\Classes\.$_\OpenWithProgids" -Force | Out-Null
     New-ItemProperty "HKCU:\SOFTWARE\Classes\.$_\OpenWithProgids" -Name "Harmonoid.$_" -Value '' -Force | Out-Null
 }
